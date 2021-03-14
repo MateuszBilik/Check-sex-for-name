a199dd02cff51860e7feb12a7577810f7e6cc146
Mateusz <74718131+MateuszBilik@users.noreply.github.com>
2021-03-14 22:34:15 +0100

    check by first name is done, tested by postman

--- src/main/java/com/business/demo/controller/RecognizeSexController.java
+++ src/main/java/com/business/demo/controller/RecognizeSexController.java
@@ -1,14 +1,23 @@
 package com.business.demo.controller;
 
+import com.business.demo.Sex;
+import com.business.demo.service.CheckSex;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class RecognizeSexController {
 
-@GetMapping("/check")
-    public String checkSex (@RequestParam String name) {
+    private final CheckSex checkSex;
 
-    return null;
+    public RecognizeSexController(CheckSex checkSex) {
+        this.checkSex = checkSex;
+    }
+
+    @GetMapping("/check")
+    public Sex checkSex (@RequestParam String name) {
+    return checkSex.checkSexByFirstName(name);
     }
 
 }
