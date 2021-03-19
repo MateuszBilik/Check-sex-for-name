2afaf5bce0c3239fc64e4194fa05280bafa9bee8
Mateusz <74718131+MateuszBilik@users.noreply.github.com>
2021-03-19 09:40:02 +0100

    new mock

---
+++ src/test/java/com/business/demo/controller/NamesControllerTest.java
@@ -0,0 +1,32 @@
+package com.business.demo.controller;
+
+import com.business.demo.service.NamesService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.OngoingStubbing;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class NamesControllerTest {
+
+    @Mock
+    NamesService namesService;
+
+    @Test
+    void returnNames() throws FileNotFoundException {
+        //given
+        String name = "female";
+        when(namesService.getFemaleNames()).thenReturn(name);
+        NamesController namesController = new NamesController(namesService);
+        //when
+        String female = namesController.returnNames("female");
+        //then
+        assertEquals(female, name);
+    }
+}
