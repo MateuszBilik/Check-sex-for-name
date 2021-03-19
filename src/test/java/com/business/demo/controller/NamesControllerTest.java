336444e6911fe63a6501ffeb6355e3a36432b104
Mateusz <74718131+MateuszBilik@users.noreply.github.com>
2021-03-19 15:08:19 +0100

    fixed and test for namesController

--- src/test/java/com/business/demo/controller/NamesControllerTest.java
+++ src/test/java/com/business/demo/controller/NamesControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.stubbing.OngoingStubbing;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.io.FileNotFoundException;
 
@@ -19,14 +20,67 @@ public class NamesControllerTest {
     NamesService namesService;
 
     @Test
-    void returnNames() throws FileNotFoundException {
+    void returnNames_whenFemale() throws FileNotFoundException {
         //given
-        String name = "female";
+        String name = "femaleName";
         when(namesService.getFemaleNames()).thenReturn(name);
         NamesController namesController = new NamesController(namesService);
         //when
         String female = namesController.returnNames("female");
         //then
-        assertEquals(female, name);
+        assertEquals(name, female);
+    }
+
+    @Test
+    void returnNames_whenMale() throws FileNotFoundException {
+        //given
+        String name = "maleName";
+        when(namesService.getMaleNames()).thenReturn(name);
+        NamesController namesController = new NamesController(namesService);
+        //when
+        String male = namesController.returnNames("male");
+        //then
+        assertEquals(name, male);
+    }
+
+    @Test
+    void returnNames_whenNothing() throws FileNotFoundException {
+        //given
+        String maleName = "maleName";
+        String femaleName = "femaleName";
+        when(namesService.getMaleNames()).thenReturn(maleName);
+        when(namesService.getFemaleNames()).thenReturn(femaleName);
+        NamesController namesController = new NamesController(namesService);
+        String response = "Female: " + femaleName + "\nMale: " + maleName;
+        //when
+        String both = namesController.returnNames("");
+        //then
+        assertEquals(response, both);
+    }
+
+    @Test
+    void returnNames_whenAnything() throws FileNotFoundException {
+        //given
+        String maleName = "maleName";
+        String femaleName = "femaleName";
+        when(namesService.getMaleNames()).thenReturn(maleName);
+        when(namesService.getFemaleNames()).thenReturn(femaleName);
+        NamesController namesController = new NamesController(namesService);
+        String response = "Female: " + femaleName + "\nMale: " + maleName;
+        //when
+        String both = namesController.returnNames("a3w4tb");
+        //then
+        assertEquals(response, both);
+    }
+
+    @Test
+    void returnNames_whenException() throws FileNotFoundException {
+        //given
+        when(namesService.getMaleNames()).thenThrow(new FileNotFoundException());
+        NamesController namesController = new NamesController(namesService);
+        //when
+        String both = namesController.returnNames("a3w4tb");
+        //then
+        assertEquals(both, "Problem with file");
     }
 }
